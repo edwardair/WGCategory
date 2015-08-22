308b90f99e18a903c6270f290eea7a5d94e49ce1
Eduoduo_RayMi <550621009@qq.com>
2015-08-22 12:52:51 +0800

    修复：JSONModel，在设置属性类型是int、BOOL类型，并且数据源为nil时的闪退BUG。但属性为char类型的，仍会闪退

--- WGCategory/WGClasses+WGCategory/WGJSONModel/NSObject+WGJSONModel.m
+++ WGCategory/WGClasses+WGCategory/WGJSONModel/NSObject+WGJSONModel.m
@@ -107,7 +107,12 @@
             }
             
             [self setValue:value forKey:propertyName_NSString];
-        }else{//value为空时，同样需要将value赋给self.xxx，以确保将self所带的数据覆盖掉
+        }else{
+            //当value==nil，并且属性类型为BOOL、int等类型时，直接赋值程序会闪退
+            //MARK：属性为 char类型的，还是会导致闪退问题
+            value = [NSString handleNetString:value];
+            
+            //value为空时，同样需要将value赋给self.xxx，以确保将self所带的数据覆盖掉
             [self setValue:value forKey:propertyName_NSString];
         }
     }
